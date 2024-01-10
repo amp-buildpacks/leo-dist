a2ccb5cec629abd98c12f190141062c96b2c0f51
lispking <lispking@qq.com>
2024-01-10 19:34:38 +0800

    issue(#1): Create buildpack for leo

---
+++ leo/detect.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright @2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package leo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/buildpacks/libcnb"
+)
+
+const (
+	PlanEntryLeo = "leo"
+)
+
+type Detect struct {
+}
+
+func (d Detect) Detect(context libcnb.DetectContext) (libcnb.DetectResult, error) {
+	found, err := d.leoProject(context.Application.Path)
+	if err != nil {
+		return libcnb.DetectResult{}, fmt.Errorf("unable to detect leo requirements\n%w", err)
+	}
+
+	if !found {
+		return libcnb.DetectResult{Pass: false}, nil
+	}
+
+	return libcnb.DetectResult{
+		Pass: true,
+		Plans: []libcnb.BuildPlan{
+			{
+				Provides: []libcnb.BuildPlanProvide{
+					{Name: PlanEntryLeo},
+				},
+				Requires: []libcnb.BuildPlanRequire{
+					{Name: PlanEntryLeo},
+				},
+			},
+		},
+	}, nil
+}
+
+func (d Detect) leoProject(appDir string) (bool, error) {
+	_, err := os.Stat(filepath.Join(appDir, "program.json"))
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	if err != nil {
+		return false, fmt.Errorf("unable to determine if program.json exists\n%w", err)
+	}
+
+	return true, nil
+}
---
+++ leo/leo.go
@@ -0,0 +1,126 @@
+/*
+ * Copyright @2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package leo
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/buildpacks/libcnb"
+	"github.com/paketo-buildpacks/libpak"
+	"github.com/paketo-buildpacks/libpak/bard"
+	"github.com/paketo-buildpacks/libpak/crush"
+	"github.com/paketo-buildpacks/libpak/effect"
+	"github.com/paketo-buildpacks/libpak/sbom"
+	"github.com/paketo-buildpacks/libpak/sherpa"
+)
+
+// Leo will handle installing the `leo` tool & adding it to the PATH
+type Leo struct {
+	LayerContributor libpak.DependencyLayerContributor
+	Logger           bard.Logger
+	Executor         effect.Executor
+}
+
+func NewLeo(dependency libpak.BuildpackDependency, cache libpak.DependencyCache) Leo {
+	contributor := libpak.NewDependencyLayerContributor(dependency, cache, libcnb.LayerTypes{
+		Build:  true,
+		Cache:  true,
+		Launch: true,
+	})
+	return Leo{
+		LayerContributor: contributor,
+		Executor:         effect.NewExecutor(),
+	}
+}
+
+func (r Leo) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
+	r.LayerContributor.Logger = r.Logger
+	return r.LayerContributor.Contribute(layer, func(artifact *os.File) (libcnb.Layer, error) {
+		bin := filepath.Join(layer.Path, "bin")
+
+		r.Logger.Bodyf("Expanding %s to %s", artifact.Name(), bin)
+		if err := crush.Extract(artifact, bin, 0); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to expand %s\n%w", artifact.Name(), err)
+		}
+
+		file := filepath.Join(bin, "leo")
+		r.Logger.Bodyf("Setting %s as executable", file)
+		if err := os.Chmod(file, 0755); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to chmod %s\n%w", file, err)
+		}
+
+		r.Logger.Bodyf("Setting %s in PATH", bin)
+		if err := os.Setenv("PATH", sherpa.AppendToEnvVar("PATH", ":", bin)); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to set $PATH\n%w", err)
+		}
+
+		buf := &bytes.Buffer{}
+		if err := r.Executor.Execute(effect.Execution{
+			Command: file,
+			Args:    []string{"-V"},
+			Stdout:  buf,
+			Stderr:  buf,
+		}); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("error executing '%s -V':\n Combined Output: %s: \n%w", file, buf.String(), err)
+		}
+		ver := strings.Split(strings.TrimSpace(buf.String()), " ")
+		r.Logger.Bodyf("Checking %s version: %s", file, ver[1])
+
+		sbomPath := layer.SBOMPath(libcnb.SyftJSON)
+		dep := sbom.NewSyftDependency(layer.Path, []sbom.SyftArtifact{
+			{
+				ID:      "leo",
+				Name:    "Leo",
+				Version: ver[1],
+				Type:    "UnknownPackage",
+				FoundBy: "amp-buildpacks/leo",
+				Locations: []sbom.SyftLocation{
+					{Path: "amp-buildpacks/leo/leo/leo.go"},
+				},
+				Licenses: []string{"GNU"},
+				CPEs:     []string{fmt.Sprintf("cpe:2.3:a:leo:leo:%s:*:*:*:*:*:*:*", ver[1])},
+				PURL:     fmt.Sprintf("pkg:generic/leo@%s", ver[1]),
+			},
+		})
+		r.Logger.Debugf("Writing Syft SBOM at %s: %+v", sbomPath, dep)
+		if err := dep.WriteTo(sbomPath); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to write SBOM\n%w", err)
+		}
+		return layer, nil
+	})
+}
+
+func (r Leo) BuildProcessTypes(enableProcess string) ([]libcnb.Process, error) {
+	processes := []libcnb.Process{}
+
+	if enableProcess == "true" {
+		processes = append(processes, libcnb.Process{
+			Type:    "web",
+			Command: "leo run",
+			Default: true,
+		})
+	}
+	return processes, nil
+}
+
+func (r Leo) Name() string {
+	return r.LayerContributor.LayerName()
+}
